357b54084a28e44133d65270aecda17d9f2b4fe7
Patrick Matos <pmatos96@hotmail.com>
2021-11-02 16:15:10 -0300

    Stars shining

--- index.js
+++ index.js
@@ -2,9 +2,76 @@
 animateSun(10000);
 animateMoon(10000);
 animateSky(10000);
+createStars(150);
+
 
 
 // ---------------------- Functions
+
+function createStars(numberOfStars){
+
+
+    for(let i=1; i < numberOfStars; i++){
+        $('.starsContainer').append(getStarHtml())
+    }
+
+    animateStars();
+}
+
+function animateStars(){
+
+    anime({
+        targets: '.star',
+        scale: [1, 2],
+        delay: function(el, i){ return (i % 3)*250; },
+        duration: 500,
+        brightness: [1, 5],
+        direction: 'alternate',
+        easing: 'linear',
+        loop: true,
+        // autoPlay: false
+    })
+
+    anime({
+        targets: '.star',
+        opacity: [
+            {
+                value: [0, 1],
+                delay: 8000,
+                duration: 2000
+            },
+            {
+                value: [1, 0],
+                delay: 9000,
+                duration: 1000
+            }
+        ],
+        loop: true
+    })
+}
+
+function getStarHtml(){
+
+    let rightDistance = getPosition();
+    let topDistance = getPosition();
+
+    return '<div class="star" style="right: ' + rightDistance.x + 'vw; top: ' + topDistance.y + '%;"></div>'
+}
+
+function getPosition(){
+    let position = {}
+
+    position.x = getRandomIntInclusive(1, 100);
+    position.y = getRandomIntInclusive(1, 100);
+
+    return position;
+}
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function animateSun(durationOfDay){
 
@@ -52,9 +119,13 @@ function animateMoon(durationOfNight){
         ],
         scale: [
             {value: [1, 0.75], duration: durationOfNight/2, easing: 'linear'},
+            {value: [0.75, 1], duration: durationOfNight/2, easing: 'linear'},
+            {value: [1, 0.75], duration: durationOfNight/2, easing: 'linear'},
             {value: [0.75, 1], duration: durationOfNight/2, easing: 'linear'}
         ],
         backgroundColor: [
+            {value: '#ffffff', duration: durationOfNight/2, easing: 'linear'},
+            {value: '#bbbbbb', duration: durationOfNight/2, easing: 'linear'},
             {value: '#ffffff', duration: durationOfNight/2, easing: 'linear'},
             {value: '#bbbbbb', duration: durationOfNight/2, easing: 'linear'}
         ],
